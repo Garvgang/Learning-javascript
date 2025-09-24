d8aa1c71b63611cf4b6c51fa3be93b6b73884193
Garvgang <149189049+Garvgang@users.noreply.github.com>
2025-09-24 06:18:13 +0000

    Variables done

---
+++ 01_basics/01Variables.js
@@ -0,0 +1,7 @@
+const accountId=2315
+let accountemail="garvgang2004@gmail.com"
+var accountPassword="135263"
+accountCity="Jaipur"
+let accountState;
+console.table([accountId,accountemail,accountPassword,accountCity,accountState
+])
