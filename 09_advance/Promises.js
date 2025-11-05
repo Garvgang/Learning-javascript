fb7b9e1f609edcb3b289bc73745beea7d178efcf
Garvgang <149189049+Garvgang@users.noreply.github.com>
2025-11-05 18:35:06 +0000

    done

--- 09_advance/Promises.js
+++ 09_advance/Promises.js
@@ -28,45 +28,72 @@
 // promisethree.then(function(user){
 //     console.log(user);
 // })
-const promisefour=new Promise(function(resolve,reject){
-    setTimeout(function(){
-        let error=true;
-        if(!error){
-            resolve({username:"chai",password:"123"})
-        }
-        else{
-            reject("ERROR:Something went wrong")
-        }
-    },1000)
-})
-promisefour
-.then((user)=>{
-    console.log(user);
-    return user.username;
-})
-.then((username)=>{
-    console.log(username);
-})
-.catch(function(error){
-    console.log(error);
-})
-.finally(()=>console.log("The promise is either resolved or completed"))
+// const promisefour=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error=true;
+//         if(!error){
+//             resolve({username:"chai",password:"123"})
+//         }
+//         else{
+//             reject("ERROR:Something went wrong")
+//         }
+//     },1000)
+// })
+// promisefour
+// .then((user)=>{
+//     console.log(user);
+//     return user.username;
+// })
+// .then((username)=>{
+//     console.log(username);
+// })
+// .catch(function(error){
+//     console.log(error);
+// })
+// .finally(()=>console.log("The promise is either resolved or completed"))
 
-const promisefive=new Promise(function(resolve,reject){
-    setTimeout(function(){
-        let error=false;
-        if(!error){
-            resolve({username:"Javascript",password:"123"})
-        }
-        else{
-            reject("ERROR :JS WENT WRONG")
-        }
-    },1000);
+// const promisefive=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error=true;
+//         if(!error){
+//             resolve({username:"Javascript",password:"123"})
+//         }
+//         else{
+//             reject("ERROR :JS WENT WRONG")
+//         }
+//     },1000);
 
+// })
+// // problem of this approach is that it cannot directly resolve this 
+// async function consumePromiseFive(){
+//     try{
+//         const response=await promisefive;
+//         console.log(response);
+//     }
+//     catch(error){
+//         console.log(error);
+//     }
+// }
+// consumePromiseFive()
+
+// async function getAllUsers() {
+//     try {
+//         const response=await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data=response.json();
+//         console.log(data); 
+//     } catch (error) {
+//         console.log("E: ",error);
+//     }   
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
 })
-// problem of this approach is that it cannot directly resolve this 
-async function consumePromiseFive(){
-    const response=await promisefive;
-    console.log(response);
-}
-consumePromiseFive()
+.then((data)=>{
+    console.log(data);
+    
+})
+.catch((error)=> console.log(error))
